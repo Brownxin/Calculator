1b891301ec801da17fcc4842d96a63207c452cfd
Xin Wang <kevin.wx86@gmail.com>
2016-02-15 17:00:02 -0500

    Model of Calculator

--- Caculator/CalculatorBrain.swift
+++ Caculator/CalculatorBrain.swift
@@ -8,7 +8,34 @@
 
 import Foundation
 
-class CalculatorBrain
-{
+class CalculatorBrain {
+    enum Op {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+    }
     
+    var opStack = [Op]()
+    
+    var knownOps = Dictionary<String, Op>()
+//    var knownOps = [String:Op]()
+    
+    init(){
+        knownOps["×"] = Op.BinaryOperation("×", {$0 * $1})
+        knownOps["÷"] = Op.BinaryOperation("/", {$1 / $0})
+        knownOps["+"] = Op.BinaryOperation("×", {$0 + $1})
+        knownOps["−"] = Op.BinaryOperation("×", {$1 - $0})
+        knownOps["√"] = Op.UnaryOperation("√", {sqrt($0)})
+    }
+    
+    func pushOperand(operand: Double){
+        opStack.append(Op.Operand(operand))
+    }
+    
+    func performOperation(symbol: String){
+        if let operation = knownOps[symbol]{
+            opStack.append(operation)
+        }
+        
+    }
 }
